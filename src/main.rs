55ad1dbd23baf6e5f0658f1581706d013ecd5a59
Yaroslav Petrov <yaroslav.v.petrov@yandex.ru>
2022-04-27 06:08:49 +0000

    Fix scheduling logic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use log::*;
-use std::sync::mpsc;
+use std::str::FromStr;
 use std::sync::Arc;
-use tokio_cron_scheduler::{Job, JobScheduler};
 
 #[tokio::main]
 async fn main() {
@@ -19,50 +18,35 @@ async fn main() {
 
     // MAIN LOOP DEFITIONS
     let config = Arc::pin(tokio::sync::Mutex::new(config::init()));
-    let (snd, rcv) = mpsc::channel::<()>();
-    let snd = Arc::pin(std::sync::Mutex::new(snd));
-
-    let backup = {
+    {
         let config = config.clone();
         async move {
-            // BACKUP THREAD
-            std::thread::spawn(|| async move {
-                let drive = drive_backup::DriveBackup::new(config).await;
-                loop {
-                    debug!("Awaiting for the next backup call");
-                    rcv.recv().unwrap();
-                    debug!("Backup call received");
-                    {
-                        trace!("Calling `drive.backup_changes`");
-                        drive.backup_changes().await;
-                        trace!("Finished `drive.backup_changes`");
-                    }
-                }
-            })
-            .join()
-            .unwrap()
-            .await
-        }
-    };
-
-    let scheduler_config = config;
-    let scheduler = async {
-        // SCHEDULER THREAD
-        let config = scheduler_config;
-        let mut sched = JobScheduler::new();
-        sched
-            .add(
-                Job::new(config.lock().await.backup_cron.as_str(), move |_, _| {
-                    debug!("Scheduler calls backup");
-                    snd.lock().unwrap().send(()).unwrap()
+            // BACKUP ROUTINE
+            let backup_cron = cron::Schedule::from_str(&config.lock().await.backup_cron).unwrap();
+            let drive = drive_backup::DriveBackup::new(config).await;
+            loop {
+                futures_timer::Delay::new({
+                    let wait_duration = (backup_cron.upcoming(chrono::Utc).next().unwrap()
+                        - chrono::Utc::now())
+                    .to_std()
+                    .unwrap();
+                    debug!(
+                        "Awaiting for the next backup call (will trigger in {} seconds)",
+                        wait_duration.as_secs()
+                    );
+                    wait_duration
                 })
-                .unwrap(),
-            )
-            .unwrap();
-        sched.start().await.unwrap()
-    };
-
-    futures::join!(backup, scheduler);
+                .await;
+                debug!("Backup call received");
+                {
+                    trace!("Calling `drive.backup_changes`");
+                    drive.backup_changes().await;
+                    trace!("Finished `drive.backup_changes`");
+                }
+            }
+        }
+    }
+    .await;
 }
 
 pub mod config;
